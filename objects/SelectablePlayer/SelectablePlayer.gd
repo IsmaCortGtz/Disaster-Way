389425f2e8bb2264a37269c117380ad5b423e205
IsmaCortGtz <ismacortgtz@gmail.com>
2023-04-26 16:32:35 -0600

    Buttons Icons added

--- objects/SelectablePlayer/SelectablePlayer.gd
+++ objects/SelectablePlayer/SelectablePlayer.gd
@@ -12,4 +12,7 @@ func label_alpha(alpha: float):
 func player_joined(index):
 	get_node("MarginContainer").visible = false
 	get_node("HBoxContainer/VBoxContainer/Player Num").text = "Jugador " + str(index)
+	get_node("HBoxContainer/VBoxContainer/MarginContainer/Player-image").modulate = GameInput.playerColors[index - 1]
+	get_node("HBoxContainer/CenterContainer/Player-left").self_modulate = GameInput.playerColors[index - 1]
+	get_node("HBoxContainer/CenterContainer2/Player-right").self_modulate = GameInput.playerColors[index - 1]
 	get_node("HBoxContainer").visible = true
